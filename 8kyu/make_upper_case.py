9e0188911b5ca2b2fd2818294d0377fac97a4ab9
Iván E. Hernández T <iamieht@gmail.com>
2019-10-09 17:59:31 +0200

    8kyu - make_upper_case

---
+++ 8kyu/make_upper_case.py
@@ -0,0 +1,10 @@
+def make_upper_case(s):
+    '''
+    String -> String
+    produces an upper cased string
+    '''
+    return s.upper()
+
+# Test
+print('Expected result:', 'HELLO')
+print('Actual result:', make_upper_case('hello'))
