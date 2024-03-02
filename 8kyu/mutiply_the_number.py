b2a1ba3e0e48aac6e0bb0335fc2a007fca51e50f
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-03-02 20:44:02 +0100

    multiply the number

---
+++ 8kyu/mutiply_the_number.py
@@ -0,0 +1,19 @@
+# Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+
+#   3 -->    15  (  3 * 5¹)
+#  10 -->   250  ( 10 * 5²)
+# 200 --> 25000  (200 * 5³)
+#   0 -->     0  (  0 * 5¹)
+#  -3 -->   -15  ( -3 * 5¹)
+def multiply(n):
+    power = len(str(abs(n)))
+
+    return n * 5 ** power
+
+
+# Examples
+print(multiply(3))  # 15
+print(multiply(10))  # 250
+print(multiply(200))  # 25000
+print(multiply(0))  # 0
+print(multiply(-3))  # -15
